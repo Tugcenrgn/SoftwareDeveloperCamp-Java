0179387022ae9a760e165e08447ed94374dbcd5a
tuğçe nur ergen <60977455+Tugcenrgn@users.noreply.github.com>
2021-04-30 17:57:48 +0300

    Create Instructor.java

---
+++ Hw2/Instructor.java
@@ -0,0 +1,17 @@
+package hw2;
+
+public class Instructor {
+
+	int instructorId;
+	String instructorName;
+	
+	public Instructor() {
+		instructorId = 0;
+		instructorName = null;
+	}
+	
+	public Instructor(int instructorId , String instructorName) {
+		this.instructorId = instructorId ;
+		this.instructorName = instructorName;
+	}
+}
