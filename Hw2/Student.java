c00cddc5f4433ae205cd2e7a3c9ef886483fcd34
tuğçe nur ergen <60977455+Tugcenrgn@users.noreply.github.com>
2021-04-30 17:58:34 +0300

    Create Student.java

---
+++ Hw2/Student.java
@@ -0,0 +1,11 @@
+package hw2;
+
+public class Student {
+	String studentFirstName;
+	String studentLastName;
+	
+	public Student(String firstName, String lastName) {
+		studentFirstName = firstName;
+		studentLastName = lastName;
+	}
+}
