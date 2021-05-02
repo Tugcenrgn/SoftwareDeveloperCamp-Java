868da35953d75a9b85c14143eeef10fad0c79428
tuğçe nur ergen <60977455+Tugcenrgn@users.noreply.github.com>
2021-05-02 15:58:28 +0300

    Create UserManager.java

---
+++ Day3Hw2/UserManager.java
@@ -0,0 +1,16 @@
+package kodlamaio;
+
+public class UserManager {
+	public void add(User user) {
+		System.out.println("Kullanici basariyla eklendi");
+	}
+	
+	public void delete(User user) {
+		System.out.println("Kullanici basariyla silindi");
+	}
+	
+	public void update(User user) {
+		System.out.println("Kullanici basariyla guncellendi");
+		
+	}
+}
