ee3b0bade66312c3e77bf288ee4dc76aa7b665b7
tuğçe nur ergen <60977455+Tugcenrgn@users.noreply.github.com>
2021-05-02 15:58:46 +0300

    Create Course.java

---
+++ Day3Hw2/Course.java
@@ -0,0 +1,12 @@
+package kodlamaio;
+
+public class Course {
+
+	String courseName;
+	Instructor courseInstructor;
+	
+	public Course(String courseName, Instructor courseInstructor) {
+		this.courseName = courseName;
+		this.courseInstructor = courseInstructor;
+	}
+}
