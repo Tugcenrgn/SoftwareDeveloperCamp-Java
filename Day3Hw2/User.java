d8513f254c3b157676bec46af4e788b4c38ff6ff
tuğçe nur ergen <60977455+Tugcenrgn@users.noreply.github.com>
2021-05-02 15:59:48 +0300

    Create User.java

---
+++ Day3Hw2/User.java
@@ -0,0 +1,18 @@
+package kodlamaio;
+
+public class User {
+
+	int id;
+	String email;
+	String password;
+	
+	public User() {
+		
+	}
+	public User(int id, String email, String password) {
+		this.email = email;
+		this.id = id;
+		this.password = password;
+	}
+	
+}
